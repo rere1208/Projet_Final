112fd5399385642d01721b90dcaaf25c9b0ee84e
unknown <remidescamps7@gmail.com>
2024-04-03 14:59:17 +0200

    script changer

--- JS/script.js
+++ JS/script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const playlist = document.querySelector('.playlist ul');
 
     // Fonction pour ajouter une nouvelle musique à la playlist
-    function addMusicToPlaylist(musicName, musicSrc, videoId) {
+    function addMusicToPlaylist(musicName, musicSrc) {
         // Créer un nouvel élément li
         const newMusic = document.createElement('li');
         newMusic.textContent = musicName;
@@ -20,27 +20,19 @@ document.addEventListener('DOMContentLoaded', function() {
             audioPlayer.src = musicSrc;
             audioPlayer.play();
             updateCurrentMusicName(musicName);
-            updateVideoSource(videoId);
         });
         // Ajouter le nouvel élément li à la liste de lecture
         playlist.appendChild(newMusic);
     }
 
     // Ajouter des musiques existantes à la playlist
-    addMusicToPlaylist("KINGDOM HEARTS - Dearly Beloved - All Versions 2002-2017", "upload/music/KINGDOM HEARTS - Dearly Beloved - All Versions 2002-2017.mp4", "video1");
-    addMusicToPlaylist("Dont Think Twice", "upload/music/Dont Think Twice.mp4", "video2");
-    addMusicToPlaylist("Sanctuary (Ending)", "upload/music/Sanctuary (Ending).mp4.mp4", "video3");
+    addMusicToPlaylist("KINGDOM HEARTS - Dearly Beloved - All Versions 2002-2017", "upload/music/KINGDOM HEARTS - Dearly Beloved - All Versions 2002-2017.mp4");
+    addMusicToPlaylist("Dont Think Twice", "upload/music/Dont Think Twice.mp4");
+    addMusicToPlaylist("Sanctuary (Ending)", "upload/music/Sanctuary (Ending).mp4.mp4");
 
     function updateCurrentMusicName(musicName) {
         const musicNameDisplay = document.getElementById('musicNameDisplay');
         musicNameDisplay.textContent = musicName;
-    }
-
-    // Fonction pour mettre à jour l'URL de la vidéo YouTube en fonction de l'identifiant de la vidéo
-    function updateVideoSource(videoId) {
-        const videoPlayer = document.getElementById('videoPlayer');
-        const newSrc = `https://www.youtube.com/embed/${videoId}`;
-        videoPlayer.src = newSrc;
     }
 
     // Ajouter un événement au bouton Play
